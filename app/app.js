5ba64962e7af2ff7807d3f267911cb6b09bf4bab
cb-manoji <manoji@chargebee.com>
2022-05-09 14:41:39 +0530

    Updated platform version, added region install param, update the code for region.

--- app/app.js
+++ app/app.js
@@ -4,17 +4,50 @@ $(document).ready( function() {
         var client = _client;
         client.events.on('app.activated',
           function() {
-              client.data.get('contact')
-                  .then(function(data) {
-                      var inst = pluginInit.call(window, jQuery);
-                      inst.execute(data.contact.email);
-                  })
-                  .catch(function(e) {
-                      console.log('Exception - ', e);
-                  });
+              getRegion(client).then(
+                  function(region) {
+                      console.log(region);
+                      getEmail(client).then(
+                          function(email){
+                            console.log("email " + email);
+                            var inst = pluginInit.call(window, jQuery);
+                            inst.execute(email, region);
+                          }
+                      ).catch(function(error) {
+                        console.log('Exception - ', error);
+                      })
+                  }
+              )
       });
   });
 });
+
+function getRegion(client) {
+    return new Promise(function(resolve, reject) {
+        client.iparams.get("region").then(
+            function(data) {
+                resolve(data["region"].toLowerCase())
+              },
+            function(error) {
+                resolve("US".toLowerCase())
+            }
+        )
+    })
+}
+
+function getEmail(client) {
+    return new Promise(function(resolve, reject) {
+        client.data.get('contact').then(
+            function(data) {
+                resolve(data.contact.email)
+              },
+            function(error) {
+                reject(error)
+            }
+        )
+    })
+}
+
 var pluginInit = function($) {
   'use strict';
   var requestTimeOut = null;
@@ -22,13 +55,15 @@ var pluginInit = function($) {
   var cbPath, detailsUrl;
   var window = this;
 
-  function _execute(email) {
+  function _execute(email, region) {
       var _env = {
           site: 'app',
+          region: region,
           hostSuffix: '.chargebee.com',
           protocol: "https"
       };
-      cbPath = _env.protocol + "://" + _env.site + _env.hostSuffix;
+      cbPath = _env.protocol + "://" + _env.site + `.${_env.region}` + _env.hostSuffix;
+      console.log(cbPath);
       detailsUrl = cbPath + _path + encodeURIComponent(email);
       $('#request-error').hide();
       loadSummary();
